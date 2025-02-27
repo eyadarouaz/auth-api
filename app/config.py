4318d6ed830225f578f8fdd8571e5e5d503c8742
Eya Darouaz <eya.darouaz@episousse.com.tn>
2025-02-27 19:17:21 +0000

    Read database string from env file

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "db_string")
+    DATABASE_URI: str = os.getenv("DATABASE_URI")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
