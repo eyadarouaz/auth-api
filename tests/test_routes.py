8c4db191c43bf36083b97f1a91623a493529ca6e
eyadarouaz <eyaadarouez@gmail.com>
2025-03-28 23:57:26 +0100

    Fix postgres test db for github actions

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,7 +13,7 @@ BASE_URL = "/users"
 
 DATABASE_URI = os.getenv(
     "TEST_DATABASE_URI",
-    "postgresql://postgres:postgres@postgres:5432/postgres"
+    "postgresql://postgres:${{ secrets.POSTGRES_PASSWORD }}@postgres:5432/postgres"
 )
 
 engine = create_engine(DATABASE_URI, echo=True)
